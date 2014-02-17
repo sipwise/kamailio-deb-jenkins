ab0e4f760c0248acc6df8e8caf50d34dad47c28b
Michael Prokop <mprokop@sipwise.com>
2014-02-17 23:13:51 +0100

    Adapt ec2 bootstrap script for usage as root, adjust pbuilder + sudo config

--- ec2/bootstrap.sh
+++ ec2/bootstrap.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+if [ "$(id -u 2>/dev/null)" != 0 ] ; then
+  echo "Error: please execute this script as user root" >&2
+  exit 1
+fi
+
 export DEBIAN_FRONTEND=noninteractive
 
 # enable j-d-g repos
 if [ -r /etc/apt/sources.list.d/jenkins-debian-glue.list ] ; then
-  echo "/etc/apt/sources.list.d/jenkins-debian-glue.list exists already"
+  echo "!!! /etc/apt/sources.list.d/jenkins-debian-glue.list exists already !!!"
 else
-  echo "deb     http://jenkins.grml.org/debian jenkins-debian-glue main" | sudo tee -a /etc/apt/sources.list.d/jenkins-debian-glue.list
+  echo "deb     http://jenkins.grml.org/debian jenkins-debian-glue main" > /etc/apt/sources.list.d/jenkins-debian-glue.list
 fi
-wget -O - http://jenkins.grml.org/debian/C525F56752D4A654.asc | sudo apt-key add -
+
+if apt-key list | grep -q 52D4A654 ; then
+  echo "!!! GnuPG key 52D4A654 already listed in apt-key setup !!!"
+else
+  wget -O - http://jenkins.grml.org/debian/C525F56752D4A654.asc | apt-key add -
+fi
 
 if [ -r /usr/share/keyrings/ubuntu-archive-keyring.gpg ] ; then
-  echo "/usr/share/keyrings/ubuntu-archive-keyring.gpg exists already"
+  echo "!!! /usr/share/keyrings/ubuntu-archive-keyring.gpg exists already !!!"
 else
   wget -O ubuntu-keyring_2012.05.19_all.deb \
     http://de.archive.ubuntu.com/ubuntu/pool/main/u/ubuntu-keyring/ubuntu-keyring_2012.05.19_all.deb
@@ -19,22 +29,22 @@ else
 fi
 
 # make sure we use up2date packages
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 
 # packages required for building on slaves
-sudo apt-get -y install jenkins-debian-glue-buildenv jenkins-debian-glue-buildenv-taptools jenkins-debian-glue-buildenv-lintian openjdk-7-jre-headless
+apt-get -y install jenkins-debian-glue-buildenv jenkins-debian-glue-buildenv-taptools jenkins-debian-glue-buildenv-lintian openjdk-7-jre-headless
 
 # commodity packages
-sudo apt-get -y install screen zsh vim
+apt-get -y install screen zsh vim
 
 # get rid of installation packages
-sudo apt-get clean
+apt-get clean
 
 if [ -r /etc/jenkins/pbuilderrc ] ; then
-  echo "/etc/jenkins/pbuilderrc exists already"
+  echo "!!! /etc/jenkins/pbuilderrc exists already !!!"
 else
-  sudo tee -a /etc/jenkins/pbuilderrc >/dev/null <<EOF
+  cat > /etc/jenkins/pbuilderrc <<EOF
 # ccache
 CCACHEDIR=/var/cache/pbuilder/ccache
 
@@ -57,18 +67,34 @@ esac
 EOF
 fi
 
+if [ -r /etc/sudoers.d/pbuilder ] ; then
+  echo "!!! /etc/sudoers.d/pbuilder exists already !!!"
+else
+  cat > /etc/sudoers.d/pbuilder <<EOF
+Defaults  env_keep+="DEB_* release branch distribution JOB_NAME"
+EOF
+fi
+
+if grep -q '^PBUILDER_CONFIG=' /etc/jenkins/debian_glue 2>/dev/null ; then
+  echo "!!! /etc/jenkins/debian_glue with PBUILDER_CONFIG exists already !!!"
+else
+  echo "PBUILDER_CONFIG=/etc/jenkins/pbuilderrc" >> /etc/jenkins/debian_glue
+fi
+
 for distri in jessie lenny lucid precise squeeze wheezy ; do
+  export distribution=$distri # for usage in pbuilderrc
+
   arch=amd64
   if [ -d /var/cache/pbuilder/base-${distri}-${arch}.cow ] ; then
-    echo "/var/cache/pbuilder/base-${distri}-${arch}.cow exists already"
+    echo "!!! /var/cache/pbuilder/base-${distri}-${arch}.cow exists already !!!"
   else
-    sudo cowbuilder --create --basepath /var/cache/pbuilder/base-${distri}-${arch}.cow --distribution ${distri} --debootstrapopts --arch --debootstrapopts ${arch} --debootstrapopts --variant=buildd --configfile=/etc/jenkins/pbuilderrc
+    cowbuilder --create --basepath /var/cache/pbuilder/base-${distri}-${arch}.cow --distribution ${distri} --debootstrapopts --arch --debootstrapopts ${arch} --debootstrapopts --variant=buildd --configfile=/etc/jenkins/pbuilderrc
   fi
 
   arch=i386
   if [ -d /var/cache/pbuilder/base-${distri}-${arch}.cow ] ; then
-    echo "/var/cache/pbuilder/base-${distri}-${arch}.cow exists already"
+    echo "!!! /var/cache/pbuilder/base-${distri}-${arch}.cow exists already !!!"
   else
-    sudo cowbuilder --create --basepath /var/cache/pbuilder/base-${distri}-${arch}.cow --distribution ${distri} --debootstrapopts --arch --debootstrapopts ${arch} --debootstrapopts --variant=buildd --configfile=/etc/jenkins/pbuilderrc
+    cowbuilder --create --basepath /var/cache/pbuilder/base-${distri}-${arch}.cow --distribution ${distri} --debootstrapopts --arch --debootstrapopts ${arch} --debootstrapopts --variant=buildd --configfile=/etc/jenkins/pbuilderrc
   fi
 done
