21ef9b4a24d94b23ea142eb8ac1c0bcaeff614c8
Michael Prokop <mprokop@sipwise.com>
2014-02-18 09:10:37 +0100

    ec2: install jenkins-debian-glue-buildenv-piuparts

--- ec2/bootstrap.sh
+++ ec2/bootstrap.sh
@@ -33,8 +33,7 @@ apt-get update
 apt-get -y upgrade
 
 # packages required for building on slaves
-apt-get -y install jenkins-debian-glue-buildenv jenkins-debian-glue-buildenv-taptools jenkins-debian-glue-buildenv-lintian
-openjdk-7-jre-headless ntp
+apt-get -y install jenkins-debian-glue-buildenv jenkins-debian-glue-buildenv-taptools jenkins-debian-glue-buildenv-lintian jenkins-debian-glue-buildenv-piuparts openjdk-7-jre-headless ntp
 
 # commodity packages
 apt-get -y install screen zsh vim
