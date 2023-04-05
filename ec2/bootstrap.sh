76bc68d907e3b7782352e45a44a815938bc8b563
Michael Prokop <mprokop@sipwise.com>
2023-04-05 16:01:03 +0200

    ec2: also jessie/security is now at archive.debian.org
    Change-Id: I13e5dc9534bb3d131ff27d6ba798e9c0c4aff71d

--- ec2/bootstrap.sh
+++ ec2/bootstrap.sh
@@ -258,7 +258,7 @@ case "$distribution" in
     # nowadays resides on archive
     MIRRORSITE="http://archive.debian.org/debian/"
     # security and updates
-    OTHERMIRROR="deb http://security.debian.org/debian-security ${distribution}/updates main"
+    OTHERMIRROR="deb http://archive.debian.org/debian-security ${distribution}/updates main"
     # we need key id CBF8D6FD518E17E1
     DEBOOTSTRAPOPTS=("${DEBOOTSTRAPOPTS[@]}" "--keyring=/usr/share/keyrings/debian-archive-removed-keys.gpg")
     # support bootstrapping archived repository with expired GPG key
